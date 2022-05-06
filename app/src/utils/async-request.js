c9d4a142467e0df752db67a2160f2c76279013e0
Funbi Oyede <oyedetobi49@gmail.com>
2022-05-06 10:30:19 +0100

    go

--- app/src/utils/async-request.js
+++ app/src/utils/async-request.js
@@ -5,6 +5,7 @@ module.exports = (handler) => (request, response) =>
   handler(request, response).catch((error) => {
    logger.error(error.message, error.status, error);
     if (error instanceof Error) {
+    console.log(error)
       return response.status(error.statusCode).json({
         status: "error",
         code: error.statusCode,
