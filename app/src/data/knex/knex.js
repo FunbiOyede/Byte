57d563b8090932b37b3e097f53a75ade24481271
Funbi Oyede <oyedetobi49@gmail.com>
2022-06-04 16:23:32 +0100

    Bug fixes

--- app/src/data/knex/knex.js
+++ app/src/data/knex/knex.js
@@ -1,6 +1,6 @@
 const environment = process.env.ENVIRONMENT || 'development'
 const config = require('../../../../knexfile')[environment];
-const { logger } = require("../utils/logger")
+const { logger } = require("../../utils/logger")
 logger.info( environment,"environment")
 
 module.exports = require('knex')(config);
