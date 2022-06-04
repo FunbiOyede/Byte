0fda64fad0ab28836a2b856892f479d6ea8b6dae
Funbi Oyede <oyedetobi49@gmail.com>
2022-06-04 15:51:55 +0100

    Bug Fixes

--- app/src/data/knex/knex.js
+++ app/src/data/knex/knex.js
@@ -1,3 +1,4 @@
 const environment = process.env.ENVIRONMENT || 'development'
 const config = require('../../../../knexfile')[environment];
+console.log( environment,"checking environment")
 module.exports = require('knex')(config);
--- app/src/utils/async-request.js
+++ app/src/utils/async-request.js
@@ -5,14 +5,14 @@ module.exports = (handler) => (request, response) =>
   handler(request, response).catch((error) => {
    logger.error(error.message, error.status, error);
     if (error instanceof Error) {
-      console.log( error)
-      // return response.status(error.statusCode).json({
-      //   status: "error",
-      //   code: error.statusCode,
-      //   success: false,
-      //   message: error.message,
-      //   error
-      // });
+    
+      return response.status(error.statusCode).json({
+        status: "error",
+        code: error.statusCode,
+        success: false,
+        message: error.message,
+        error
+      });
     } else if (error instanceof BadRequest) {
       return response.status(error.statusCode).json({
         status: "error",
